99a388b18ca634595be83cbf55d21727c49ddd6f
erlingrj <erling.jellum@gmail.com>
2017-01-12 12:56:30 +0100

    Update oving1.go

--- oving1.go
+++ oving1.go
@@ -10,29 +10,23 @@ var i int = 0
 
 func thread1(ch chan int){
 	for j := 0; j < 1000000; j++{
-		i :=<-ch
 		i++
-		ch <-i
 	}
 }
 
 func thread2(ch chan int){
 	for j := 0; j < 1000000; j++{
-		i := <-ch
 		i--
-		ch <-i
 	}
 }
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU()) 
-	ch :=make(chan int, 1)
-	ch <- 0
 
-	go thread1(ch)
+	go thread1()
 	//time.Sleep(100*time.Millisecond)
 
-	go thread2(ch)
+	go thread2()
 	time.Sleep(100*time.Millisecond)
   	Println(i)
 }
